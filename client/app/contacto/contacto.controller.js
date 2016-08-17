f4efc84f0456464adb2835d5f6e4b725cc0149f6
Esteban Muruzabal <esteban.muruzabal@wnco.com>
2016-08-17 15:16:41 -0500

    de todo en la ofi

--- client/app/contacto/contacto.controller.js
+++ client/app/contacto/contacto.controller.js
@@ -1,10 +1,28 @@
 'use strict';
 
 angular.module('clubEstudiantesApp')
-  .controller('ContactoCtrl', function ($scope) {
+  .controller('ContactoCtrl', function ($scope, $http) {
+   $scope.postData = {};
 
-    $scope.message = 'Hello';
 
-    $scope.contact.name;
+    //http://stackoverflow.com/questions/26326027/sending-email-with-node-mailer-and-sendgrid-in-angular-mean-stack-using-angular
+    //http://stackoverflow.com/questions/27861722/creating-a-contact-form-in-angular-with-nodemailer
+
+    $scope.postMail = function (contact) {
+      // Check form validation
+      if ($scope.contactForm.$invalid === true) {
+        return
+      }
+      // wrap all your input values in $scope.postData
+      $scope.postData = angular.copy(contact);
+
+      $http.post('/api/contact', $scope.postData)
+        .success(function(data) {
+          // Show success message
+        })
+        .error(function(data) {
+          // Show error message
+        });
+    };
 
   });
