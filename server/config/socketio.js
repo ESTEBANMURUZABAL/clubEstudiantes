a1fc1ea78951c3b2e1e46cae7004ff44a02fdb0e
Esteban <esteban.muruzabal@globant.com>
2016-08-18 15:29:52 -0300

    added contacto endpoint

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -17,6 +17,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/contacto/contacto.socket').register(socket);
   require('../api/equipo/equipo.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 
